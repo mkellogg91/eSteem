a3c2b9dcac411facee59e6a00148267cbb6cb5de
mike kellogg <mkellogg91@gmail.com>
2020-01-13 20:56:58 -0500

    initial addition of sass

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import {Provider} from 'react-redux'
 import { createStore } from 'redux';
 import rootReducer from './reducers/testReducer';
